d209b76e76f43020020fe815705de6259dcbdba4
Mikhail_Shell <theoutstandingme@gmail.com>
2025-09-25 00:25:19 +0700

    Verbosed lab1

--- lab1.py
+++ lab1.py
@@ -1,3 +1,4 @@
+import sys
 from typing import Callable, Tuple
 
 phi = (1 + 5 ** 0.5) / 2
@@ -28,6 +29,8 @@ def goldenSection(f: Callable[[float], float], l: float, r: float, e: float, ver
             x2 = b - resphi * (b - a)
             f2 = f(x2)
         count += 1
+    print(f"количество итераций {arg}: {count}")
+    print(f"{arg} = [{x1}; {x2}]")
     min = (a + b) / 2.0
     return min, count
 
@@ -41,7 +44,7 @@ def goldenSectionXY(f: Callable[[float, float],float], l: float, r: float, e: fl
         x2 = b - resphi * (b - a)
         print(f"номер итерации X: {count_x}")
         print(f"X = [{x1}; {x2}]")
-        y1_opt, cy1 = goldenSection(lambda y: f(x1, y), l, r, e, True, 'Y')
+        y1_opt, cy1 = goldenSection(lambda y: f(x1, y), l, r, e, False, 'Y')
         y2_opt, cy2 = goldenSection(lambda y: f(x2, y), l, r, e, False,'Y')
         sum_y += cy1 + cy2
         if f(x1, y1_opt) <= f(x2, y2_opt):
@@ -50,21 +53,23 @@ def goldenSectionXY(f: Callable[[float, float],float], l: float, r: float, e: fl
             a = x1
         count_x += 1
     x = (a + b) / 2.0
-    y, cy_final = goldenSection(lambda y: f(x, y), l, r, e, True, 'Y')
+    y, cy_final = goldenSection(lambda y: f(x, y), l, r, e, False, 'Y')
     sum_y += cy_final
     return x, y, count_x + sum_y
 
-e = 10**(-6)
-
-# f = lambda x, y: x ** 2 - 6 * x + y ** 2 - 8 * y
-# x_min, y_min, total_count = goldenSectionXY(f, -10, 10, e)
-# print(f"x_min = {x_min}")
-# print(f"y_min = {y_min}")
-# print(f"f_min = {f(x_min, y_min)}")
-# print(f"total_count = {total_count}")
-
-f = lambda x: x ** 2 - 6 * x
-x_min, total_count = goldenSection(f, -10, 10, e, True)
-print(f"x_min = {x_min}")
-print(f"f_min = {f(x_min)}")
-print(f"total_count = {total_count}")
+e_values = [10**(-6), 10**(-8), 10**(-10)]
+for e in e_values:
+    print(f"Для e = {e}")
+    if len(sys.argv) < 2 or sys.argv[1] == "1":
+        f = lambda x: x ** 2 - 6 * x
+        x_min, total_count = goldenSection(f, -10, 10, e, True)
+        print(f"x_min = {x_min}")
+        print(f"f_min = {f(x_min)}")
+        print(f"total_count = {total_count}")
+    else:
+        f = lambda x, y: x ** 2 - 6 * x + y ** 2 - 8 * y
+        x_min, y_min, total_count = goldenSectionXY(f, -10, 10, e)
+        print(f"x_min = {x_min}")
+        print(f"y_min = {y_min}")
+        print(f"f_min = {f(x_min, y_min)}")
+        print(f"total_count = {total_count}")
