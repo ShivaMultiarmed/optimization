6a8a1004b7c85e49117d991ac551401b467616bc
mikhail_shell <theoutstandingme@gmail.com>
2025-10-02 00:04:33 +0700

    Solved lab3

---
+++ lab1-2.py
@@ -0,0 +1,75 @@
+import sys
+from typing import Callable, Tuple
+
+phi = (1 + 5 ** 0.5) / 2
+resphi = 2 - phi
+
+def goldenSection(f: Callable[[float], float], l: float, r: float, e: float, verbose: bool = False, arg: str = 'X') -> Tuple[float, float]:
+    a = l
+    b = r
+    x1 = a + resphi * (b - a)
+    x2 = b - resphi * (b - a)
+    f1 = f(x1)
+    f2 = f(x2)
+    count = 0
+    while abs(b - a) > e:
+        if verbose:
+            print(f"номер итерации {arg}: {count}")
+            print(f"{arg} = [{x1}; {x2}]")
+        if f1 < f2:
+            b = x2
+            x2 = x1
+            f2 = f1
+            x1 = a + resphi * (b - a)
+            f1 = f(x1)
+        else:
+            a = x1
+            x1 = x2
+            f1 = f2
+            x2 = b - resphi * (b - a)
+            f2 = f(x2)
+        count += 1
+    print(f"количество итераций {arg}: {count}")
+    print(f"{arg} = [{x1}; {x2}]")
+    min = (a + b) / 2.0
+    return min, count
+
+def goldenSectionXY(f: Callable[[float, float],float], l: float, r: float, e: float) -> Tuple[float, float, float]:
+    a = l
+    b = r
+    count_x = 0
+    sum_y = 0
+    while abs(b - a) > e:
+        x1 = a + resphi * (b - a)
+        x2 = b - resphi * (b - a)
+        print(f"номер итерации X: {count_x}")
+        print(f"X = [{x1}; {x2}]")
+        y1_opt, cy1 = goldenSection(lambda y: f(x1, y), l, r, e, True, 'Y')
+        y2_opt, cy2 = goldenSection(lambda y: f(x2, y), l, r, e, False,'Y')
+        sum_y += cy1 + cy2
+        if f(x1, y1_opt) <= f(x2, y2_opt):
+            b = x2
+        else:
+            a = x1
+        count_x += 1
+    x = (a + b) / 2.0
+    y, cy_final = goldenSection(lambda y: f(x, y), l, r, e, False, 'Y')
+    sum_y += cy_final
+    return x, y, count_x + sum_y
+
+e_values = [10**(-6), 10**(-8), 10**(-10)]
+for e in e_values:
+    print(f"Для e = {e}")
+    if len(sys.argv) < 2 or sys.argv[1] == "1":
+        f = lambda x: x ** 2 - 6 * x
+        x_min, total_count = goldenSection(f, -10, 10, e, True)
+        print(f"x_min = {x_min}")
+        print(f"f_min = {f(x_min)}")
+        print(f"total_count = {total_count}")
+    else:
+        f = lambda x, y: x ** 2 - 6 * x + y ** 2 - 8 * y
+        x_min, y_min, total_count = goldenSectionXY(f, -10, 10, e)
+        print(f"x_min = {x_min}")
+        print(f"y_min = {y_min}")
+        print(f"f_min = {f(x_min, y_min)}")
+        print(f"total_count = {total_count}")
---
+++ lab3.py
@@ -0,0 +1,68 @@
+from collections.abc import Callable
+
+import numpy
+import sympy
+from sympy.abc import x, y
+
+E = 1e-4
+PHI = (1 + 5 ** 0.5) / 2.0
+RESPHI = 2 - PHI
+
+def gold(f: Callable[[float], float], a: float, b: float, e: float = E, verbose: bool = False) -> float:
+    iteration = 0
+    while abs(b - a) > e:
+        iteration += 1
+        if verbose:
+            print(f"Шаг № {iteration}")
+        x1 = a + RESPHI * (b - a)
+        x2 = b - RESPHI * (b - a)
+        if f(x1) < f(x2):
+            b = x2
+        else:
+            a = x1
+    if verbose:
+        print(f"Всего шагов: {iteration}")
+    return (a + b) / 2.0
+
+
+def solve(f):
+    prev_point = numpy.array([0, 0])
+    cur_point = numpy.array([10, 10])
+    f_dx = sympy.lambdify([x, y], sympy.diff(f, x))
+    f_dy = sympy.lambdify([x, y], sympy.diff(f, y))
+    f = sympy.lambdify([x, y], f)
+    grad = lambda point: numpy.array([f_dx(*point), f_dy(*point)])
+    cur_grad = grad(cur_point)
+    scalar = None
+    d_point = cur_point - prev_point
+    iteration = 0
+    while abs(d_point[0]) > E or abs(d_point[1]) > E:
+        print(f"Итерация № {iteration}: x={cur_point[0]:.3f} y={cur_point[1]:.3f} f={f(cur_point[0], cur_point[1]):.3f} dx={d_point[0]:.3f} dy={d_point[1]:.3f}", end="")
+        if scalar is None:
+            print()
+        else:
+            print(f" scalar={scalar:.3f}")
+        t = gold(lambda r: f(*(cur_point - r * cur_grad)), 0, 10)
+        prev_point = cur_point
+        cur_point = prev_point - t * cur_grad
+        prev_grad = cur_grad
+        cur_grad = numpy.array([f_dx(*cur_point), f_dy(*cur_point)])
+        cur_grad = grad(cur_point)
+        scalar = numpy.dot(prev_grad, cur_grad)
+        iteration += 1
+        d_point = cur_point - prev_point
+
+    print(f"Итерация № {iteration}: x={cur_point[0]:.3f} y={cur_point[1]:.3f} f={f(cur_point[0], cur_point[1]):.3f}  dx={d_point[0]:.3f} dy={d_point[1]:.3f} scalar={scalar:.3f}")
+    return cur_point
+
+if __name__ == "__main__":
+    f0 = x ** 2 - 6 * x + y ** 2 - 8 * y
+    f1 = x ** 2 + 8 * x + y ** 2 - 6 * y
+    f2 = 2 * x ** 2 + 8 * x + y ** 2 - 6 * y
+    f3 = (x ** 2 - y) ** 2 + (y - 2) ** 2
+    print(solve(f0))  # 3 4
+    print(solve(f1))  # -4 3
+    print()
+    print(solve(f2))  # -2 3
+    print()
+    print(solve(f3))  # +-sqrt(2), 2
